30e7a886c3b34954b9e9efeff287e16e07f3ae07
Parker Malachowsky <95306112+pmalacho-mit@users.noreply.github.com>
2025-11-25 13:19:18 -0800

    Remove custom build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,6 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    outDir: "not-applicable",
-  },
   server: {
     host: "0.0.0.0",
   },
